54b4d3cd8773267ea51ee0e3f3ab7f204bb5e3da
bass631 <dima.bass@mail.ru>
2022-03-03 22:14:44 +0300

    3.1.4

---
+++ src/main/java/spring_boot_security/controller/RESTController.java
@@ -0,0 +1,73 @@
+package spring_boot_security.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import spring_boot_security.model.Role;
+import spring_boot_security.model.User;
+import spring_boot_security.service.RoleService;
+import spring_boot_security.service.UserService;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api")
+public class RESTController {
+
+    private UserService userService;
+    private RoleService roleService;
+
+    @Autowired
+    public RESTController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        return allUsers != null && !allUsers.isEmpty()
+                ? new ResponseEntity<>(allUsers, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> saveUser(@RequestBody User user) {
+        Set<Role> roles = roleService.getRoleById(user.getRolesId());
+        userService.saveUser(user, roles);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+        User user = userService.getUserById(id);
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getUser(@AuthenticationPrincipal User user) {
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,140 @@
+////////////////          LINK <<USER>>         ////////////////
+function getUser() {
+    fetch("/api/user",
+        {method: 'GET', dataType: 'json'})
+        .then((res) => res.json())
+        .then((user) => {
+            let temp = "";
+            temp += `<tr>
+            <td>${user.id}</td>
+            <td>${user.username}</td>
+            <td>${user.lastName}</td>
+            <td>${user.age}</td>
+            <td>${user.email}</td>
+            <td>${user.roles.map(e => e.name).join(", ")}</td>
+            </tr>`;
+            document.getElementById("userInfo").innerHTML = temp;
+        });
+}
+
+getUser();
+
+////////////////          LINK <<ADMIN>>         ////////////////
+function getAllUsers() {
+    fetch("/api/users",
+        {method: 'GET', dataType: 'json'})
+        .then(res => res.json())
+        .then(users => {
+            let temp = '';
+            users.forEach(function (user) {
+                temp += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.username}</td>
+                <td>${user.lastName}</td>
+                <td>${user.age}</td>
+                <td>${user.email}</td>
+                <td>${user.roles.map(e => " " + e.name)}</td>
+                <td><button type="submit" onclick="modalWindowEditUser(${user.id})"
+                class="btn btn-info" data-toggle="modal" data-target="#updateUser">Edit</button></td>
+                <td><button type="submit" onclick="modalWindowDeleteUser(${user.id})" 
+                class="btn btn-danger" data-toggle="modal" data-target="#deleteUser">Delete</button></td>
+              </tr>`;
+            });
+            document.getElementById("users").innerHTML = temp;
+        });
+}
+
+getAllUsers()
+
+////////////////         MODAL WINDOW <<EDIT>>         ////////////////
+function modalWindowEditUser(id) {
+    fetch("/api/users/" + id,
+        {method: 'GET', dataType: 'json'})
+        .then(res => {
+            res.json().then(user => {
+                $('#updateId').val(user.id)
+                $('#updateUsername').val(user.username)
+                $('#updateLastName').val(user.lastName)
+                $('#updateAge').val(user.age)
+                $('#updateEmail').val(user.email)
+                $('#updatePassword').val(user.password)
+                $('#updateRoles').val(user.roles.map(e => " " + e.name))
+            })
+        })
+}
+
+////////////////          EDIT USER         ////////////////
+async function updateUser() {
+    let user = {
+        id: document.getElementById('updateId').value,
+        username: document.getElementById('updateUsername').value,
+        lastName: document.getElementById('updateLastName').value,
+        age: document.getElementById('updateAge').value,
+        email: document.getElementById('updateEmail').value,
+        password: document.getElementById('updatePassword').value,
+        rolesId: $('#updateRoles').val(),
+    }
+
+    await fetch("/api/users",
+        {
+            method: 'PUT',
+            headers: {'Accept': 'application/json', 'Content-Type': 'application/json;charset=UTF-8'},
+            body: JSON.stringify(user)
+        })
+
+    $("#updateUser .close").click();
+    getAllUsers();
+    getUser();
+
+}
+
+////////////////         MODAL WINDOW <<DELETE>>         ////////////////
+function modalWindowDeleteUser(id) {
+    fetch("/api/users/" + id,
+        {method: 'GET', dataType: 'json'})
+        .then(res => {
+            res.json().then(user => {
+                $('#deleteId').val(user.id)
+                $('#deleteUsername').val(user.username)
+                $('#deleteLastName').val(user.lastName)
+                $('#deleteAge').val(user.age)
+                $('#deleteEmail').val(user.email)
+                $('#deleteRoles').val(user.roles.map(e => " " + e.name))
+            })
+        })
+}
+
+////////////////          DELETE USER         ////////////////
+async function deleteUser() {
+    await fetch("/api/users/" + document.getElementById("deleteId").value,
+        {method: 'DELETE', dataType: 'json'})
+    $("#deleteUser .close").click();
+
+    getAllUsers();
+}
+
+////////////////          NEW USER         ////////////////
+async function addNewUser() {
+    let user = {
+        // id: document.getElementById('newId').value,
+        username: document.getElementById('newUsername').value,
+        lastName: document.getElementById('newLastName').value,
+        age: document.getElementById('newAge').value,
+        email: document.getElementById('newEmail').value,
+        password: document.getElementById('newPassword').value,
+        rolesId: $('#newRoles').val(),
+    }
+    console.log("check_1")
+    fetch("/api/users",
+        {
+            method: 'POST',
+            headers: {'Accept': 'application/json', 'Content-Type': 'application/json;charset=UTF-8'},
+            body: JSON.stringify(user)
+        }).then(r => r.status)
+    console.log("check_2")
+
+    // setTimeout(getAllUsers, 1000);
+    getAllUsers()
+    getUser()
+}
